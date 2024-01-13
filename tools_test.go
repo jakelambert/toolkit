0305c62bbdb9e79f1063fe04863889f288783f38
Jake Lambert <jake@thelamberts.ca>
2024-01-13 15:09:21 -0500

    Add CreateDirIffNotExist

--- tools_test.go
+++ tools_test.go
@@ -148,3 +148,19 @@ func TestTools_UploadOneFile(t *testing.T) {
 	// clean up downloaded file
 	_ = os.Remove(fmt.Sprintf("./testdata/uploads/%s", uploadedFile.NewFileName))
 }
+
+func TestTools_CreateDirIfNotExist(t *testing.T) {
+	var testTool Tools
+
+	err := testTool.CreateDirIfNotExist("./testdata/myDir")
+	if err != nil {
+		t.Error(err)
+	}
+
+	err = testTool.CreateDirIfNotExist("./testdata/myDir")
+	if err != nil {
+		t.Error(err)
+	}
+
+	_ = os.Remove("./testdata/myDir")
+}
