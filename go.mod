4a130882db0214ae8bf0a380a7b60377d79cab4c
Jake Lambert <jake@thelamberts.ca>
2024-01-13 12:01:23 -0500

    Added to main

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jakelambert/toolkit
+
+go 1.21.5
---
+++ tools.go
@@ -0,0 +1,25 @@
+package toolkit
+
+import "crypto/rand"
+
+const randomStringSource = "abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWYZ0123456789_+"
+
+// Tools is the type used to instantiate this module.  Any variable of this type will have access
+// to all the methods with the receiver *Tools
+type Tools struct {
+}
+
+// RandomString returns a string of random characters of length n, using randomStringSource
+// as the source for the string
+func (t *Tools) RandomString(n int) string {
+	s, r := make([]rune, n), []rune(randomStringSource)
+	y := uint64(len(r))
+
+	for i := range s {
+		p, _ := rand.Prime(rand.Reader, len(r))
+		x := p.Uint64()
+		s[i] = r[x%y]
+	}
+
+	return string(s)
+}
